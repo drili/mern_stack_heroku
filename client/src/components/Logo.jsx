16d302d0c9f9fc53002945749e85d112d87fad28
drili <DriliB@hotmail.com>
2024-10-01 15:22:17 +0200

    :lipstick: small logo animation

--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 
 const Logo = ({ lightLogo }) => {
+    const [animate, setAnimate] = useState(true);
+
+    useEffect(() => {
+        setTimeout(() => setAnimate(false), 1000);
+    }, []);
+
     return (
         <span className={`${lightLogo ? "text-white" : "text-zinc-900"} text-xl font-bold whitespace-nowrap`}>
-            TaskAlloc<span className='text-pink-700 ml-[2px] mr-[1px] underline'>8</span>or
+            TaskAlloc
+            <span
+                className={`text-pink-700 ml-[2px] mr-[1px] underline ${animate ? "animate-bounce-once" : ""}`}
+                style={{ display: 'inline-block' }}
+            >
+                8
+            </span>
+            or
         </span>
-    )
-}
+    );
+};
 
-export default Logo
+export default Logo;
