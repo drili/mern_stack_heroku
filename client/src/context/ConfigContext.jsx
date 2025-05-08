d69272a0238f38cc0289bcfdc8f7aa608957e2e5
TS-KYNETIC <ts@kynetic.dk>
2025-05-08 14:08:33 +0200

    Update ConfigContext.jsx

--- client/src/context/ConfigContext.jsx
+++ client/src/context/ConfigContext.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const baseURL = process.env.NODE_ENV === "development"
-    ? "http://localhost:8000"
+    ? "http://localhost:5000"
     : "https://taskalloc8or-f0b9361af29d.herokuapp.com"
     
 export const ConfigContext = React.createContext({ baseURL })
